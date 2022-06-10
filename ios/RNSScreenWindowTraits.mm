aa9716fc7792d0b2fa0997655d4875d6f9d6809d
Xuanping <a845326948@gmail.com>
2022-06-10 16:29:32 +0800

    fix

--- ios/RNSScreenWindowTraits.mm
+++ ios/RNSScreenWindowTraits.mm
@@ -134,51 +134,51 @@
 
 + (void)enforceDesiredDeviceOrientation
 {
-#if !TARGET_OS_TV
-  dispatch_async(dispatch_get_main_queue(), ^{
-    UIInterfaceOrientationMask orientationMask = UIInterfaceOrientationMaskAllButUpsideDown;
-#if defined(__IPHONE_OS_VERSION_MAX_ALLOWED) && defined(__IPHONE_13_0) && \
-    __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_13_0
-    if (@available(iOS 13, *)) {
-      UIWindow *firstWindow = [[[UIApplication sharedApplication] windows] firstObject];
-      if (firstWindow != nil) {
-        orientationMask = [firstWindow rootViewController].supportedInterfaceOrientations;
-      }
-    } else
-#endif
-    {
-      orientationMask = UIApplication.sharedApplication.keyWindow.rootViewController.supportedInterfaceOrientations;
-    }
-    UIInterfaceOrientation currentDeviceOrientation =
-        [RNSScreenWindowTraits interfaceOrientationFromDeviceOrientation:[[UIDevice currentDevice] orientation]];
-    UIInterfaceOrientation currentInterfaceOrientation = [RNSScreenWindowTraits interfaceOrientation];
-    UIInterfaceOrientation newOrientation = UIInterfaceOrientationUnknown;
-    if ([RNSScreenWindowTraits maskFromOrientation:currentDeviceOrientation] & orientationMask) {
-      if (!([RNSScreenWindowTraits maskFromOrientation:currentInterfaceOrientation] & orientationMask)) {
-        // if the device orientation is in the mask, but interface orientation is not, we rotate to device's orientation
-        newOrientation = currentDeviceOrientation;
-      } else {
-        if (currentDeviceOrientation != currentInterfaceOrientation) {
-          // if both device orientation and interface orientation are in the mask, but in different orientations, we
-          // rotate to device's orientation
-          newOrientation = currentDeviceOrientation;
-        }
-      }
-    } else {
-      if (!([RNSScreenWindowTraits maskFromOrientation:currentInterfaceOrientation] & orientationMask)) {
-        // if both device orientation and interface orientation are not in the mask, we rotate to closest available
-        // rotation from mask
-        newOrientation = [RNSScreenWindowTraits defaultOrientationForOrientationMask:orientationMask];
-      } else {
-        // if the device orientation is not in the mask, but interface orientation is in the mask, do nothing
-      }
-    }
-    if (newOrientation != UIInterfaceOrientationUnknown) {
-      [[UIDevice currentDevice] setValue:@(newOrientation) forKey:@"orientation"];
-      [UIViewController attemptRotationToDeviceOrientation];
-    }
-  });
-#endif
+// #if !TARGET_OS_TV
+//   dispatch_async(dispatch_get_main_queue(), ^{
+//     UIInterfaceOrientationMask orientationMask = UIInterfaceOrientationMaskAllButUpsideDown;
+// #if defined(__IPHONE_OS_VERSION_MAX_ALLOWED) && defined(__IPHONE_13_0) && \
+//     __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_13_0
+//     if (@available(iOS 13, *)) {
+//       UIWindow *firstWindow = [[[UIApplication sharedApplication] windows] firstObject];
+//       if (firstWindow != nil) {
+//         orientationMask = [firstWindow rootViewController].supportedInterfaceOrientations;
+//       }
+//     } else
+// #endif
+//     {
+//       orientationMask = UIApplication.sharedApplication.keyWindow.rootViewController.supportedInterfaceOrientations;
+//     }
+//     UIInterfaceOrientation currentDeviceOrientation =
+//         [RNSScreenWindowTraits interfaceOrientationFromDeviceOrientation:[[UIDevice currentDevice] orientation]];
+//     UIInterfaceOrientation currentInterfaceOrientation = [RNSScreenWindowTraits interfaceOrientation];
+//     UIInterfaceOrientation newOrientation = UIInterfaceOrientationUnknown;
+//     if ([RNSScreenWindowTraits maskFromOrientation:currentDeviceOrientation] & orientationMask) {
+//       if (!([RNSScreenWindowTraits maskFromOrientation:currentInterfaceOrientation] & orientationMask)) {
+//         // if the device orientation is in the mask, but interface orientation is not, we rotate to device's orientation
+//         newOrientation = currentDeviceOrientation;
+//       } else {
+//         if (currentDeviceOrientation != currentInterfaceOrientation) {
+//           // if both device orientation and interface orientation are in the mask, but in different orientations, we
+//           // rotate to device's orientation
+//           newOrientation = currentDeviceOrientation;
+//         }
+//       }
+//     } else {
+//       if (!([RNSScreenWindowTraits maskFromOrientation:currentInterfaceOrientation] & orientationMask)) {
+//         // if both device orientation and interface orientation are not in the mask, we rotate to closest available
+//         // rotation from mask
+//         newOrientation = [RNSScreenWindowTraits defaultOrientationForOrientationMask:orientationMask];
+//       } else {
+//         // if the device orientation is not in the mask, but interface orientation is in the mask, do nothing
+//       }
+//     }
+//     if (newOrientation != UIInterfaceOrientationUnknown) {
+//       [[UIDevice currentDevice] setValue:@(newOrientation) forKey:@"orientation"];
+//       [UIViewController attemptRotationToDeviceOrientation];
+//     }
+//   });
+// #endif
 }
 
 + (void)updateWindowTraits
